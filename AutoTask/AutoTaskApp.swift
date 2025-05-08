b2b352e0cb1a779e54af28d6a006df75e1676e31
Ali Ahmad Fahrezy <47922563+FahrezyAli@users.noreply.github.com>
2025-05-08 16:28:41 +0700

    Initial Commit

---
+++ AutoTask/AutoTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutoTaskApp.swift
+//  AutoTask
+//
+//  Created by Ali Ahmad Fahrezy on 08/05/25.
+//
+
+import SwiftUI
+
+@main
+struct AutoTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AutoTaskTests/AutoTaskTests.swift
@@ -0,0 +1,17 @@
+//
+//  AutoTaskTests.swift
+//  AutoTaskTests
+//
+//  Created by Ali Ahmad Fahrezy on 08/05/25.
+//
+
+import Testing
+@testable import AutoTask
+
+struct AutoTaskTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
